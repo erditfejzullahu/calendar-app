ebacbc4c8d26b85a7feceaa71e5de242c22afc8a
Erdit Fejzullahu <erditfejzullahu45@gmail.com>
2026-05-24 15:34:07 +0200

    remove gradle and build output files from the android project to clean up repository

--- android/app/.cxx/Debug/236cf3i2/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::hermesvm)
-add_library(hermes-engine::hermesvm SHARED IMPORTED)
-set_target_properties(hermes-engine::hermesvm PROPERTIES
-    IMPORTED_LOCATION "/home/erditi/.gradle/caches/9.3.1/transforms/cb853bd561b81a5d93b469ce5206c94e/transformed/hermes-android-250829098.0.10-debug/prefab/modules/hermesvm/libs/android.arm64-v8a/libhermesvm.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/home/erditi/.gradle/caches/9.3.1/transforms/cb853bd561b81a5d93b469ce5206c94e/transformed/hermes-android-250829098.0.10-debug/prefab/modules/hermesvm/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
